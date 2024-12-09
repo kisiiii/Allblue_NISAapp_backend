e0a3a4c5028580da109fda45660081be0f16ac7c
Kazumi441 <kazumi522@gmail.com>
2024-12-09 13:29:35 +0700

    connect_check検証中

--- db/connect_check.py
+++ db/connect_check.py
@@ -1,15 +1,20 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import text
 from database import engine
 
+# 環境変数の再読み込み
+load_dotenv()
 
 def check_db_connection():
     try:
+        # 環境変数の確認
+        print("SSL_CA_PATH:", os.environ.get("SSL_CA_PATH"))
         with engine.connect() as connection:
             result = connection.execute(text("SELECT 1"))
             print("接続成功:", result.scalar())
     except Exception as e:
         print("接続失敗:", e)
-
 
 if __name__ == "__main__":
     check_db_connection()
